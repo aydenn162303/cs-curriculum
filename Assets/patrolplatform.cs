553f5d94d7dd36e2d675a9f9be687eee470b4311
Ayden <anewell8331@student.pps.net>
2024-11-22 13:50:17 -0800

    platform

--- Assets/patrolplatform.cs
+++ Assets/patrolplatform.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class patrolplatform : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Transform> waypoints = new List<Transform>();
+    
+    private float _speed;
+    private int currentPoint;
+    private Vector2 targetPos;
+    private bool touchPl = false;
+    private States state;
+    public GameManager gm;
+    private GameObject player;
+
+
+    private enum States
+    {
+        patrol,
+    }
+    
+
     void Start()
     {
+        gm = FindObjectOfType<GameManager>();
+        state = States.patrol;
+        _speed = 3.1f;
+        currentPoint = 0;
+    }
+
+    
+
+
+    void Update()
+    {
+        Patrol();
         
     }
 
-    // Update is called once per frame
-    void Update()
+  
+
+    void Patrol()
     {
-        
+        transform.position =Vector2.MoveTowards(transform.position,waypoints[currentPoint].position, _speed * Time.deltaTime);
+            
+        if (Vector2.Distance(transform.position, waypoints[currentPoint].position) < 0.2f)
+        {
+            if (currentPoint < 3)
+            {
+                currentPoint += 1;
+            }
+            else
+            {
+                currentPoint = 0;
+            }
+                
+        }
     }
+    
+
 }
+
